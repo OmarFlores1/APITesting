d8ed46edd5012963a123b93ffc53dda6f1e86db0
michael47265 <michael47265@yahoo.com>
2017-04-10 17:33:46 -0400

    Update api_python.py

--- api_python.py
+++ api_python.py
@@ -1,8 +1,12 @@
-from urllib.request import urlopen
-import json
 
 # Get the dataset. Example taken from http://stackoverflow.com/questions/23049767/parsing-http-response-in-python
 # The main objective of this post is to manipulate the data retrieved and get some actionable data.
+# Omar Flores, 4/10/2017
+# Use python version 3.4.
+
+from urllib.request import urlopen
+import json
+
 url = 'http://www.quandl.com/api/v1/datasets/FRED/GDP.json'
 response = urlopen(url)
 
